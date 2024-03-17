10fc5f26f59b506b079986144e89afba9b4cb655
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-17 21:05:55 +0300

    Добавил Exercise_5_2_18

---
+++ Module_5/Exercise_5_2_18/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Exercise_5_2_18
+{
+    internal class Program
+    {
+        static int[] GetMassiveFromConsole()
+        {
+            var massive = new int[10];
+            for ( int i = 0; i < massive.Length; i++)
+            {
+                Console.Write($"Число {i+1}: ");
+                massive[i] = int.Parse( Console.ReadLine());
+            }
+            return massive;
+        }
+        static void ShowSortedMassive(int[]array, bool IsSort=false)
+        {
+            if (IsSort)
+            {
+                var temp = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int k = i + 1; k < array.Length; k++)
+                    {
+                        if (array[i] > array[k])
+                        {
+                            temp = array[i];
+                            array[i] = array[k];
+                            array[k] = temp;
+                        }
+                    }
+                }
+            }
+            foreach(var item in array)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            var array = GetMassiveFromConsole();
+            ShowSortedMassive(array,true);
+        }
+    }
+}
