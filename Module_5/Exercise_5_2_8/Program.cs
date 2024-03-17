b1a7d12b71ed11c2d818be63b859671a052c018d
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-17 19:49:52 +0300

    Добавил Exercise_5_2_8

---
+++ Module_5/Exercise_5_2_8/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Exercise_5_2_8
+{
+    internal class Program
+    {
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.Write($"Введите число {i + 1}: ");
+                result[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("\nПоказать массив");
+            Console.ReadKey();
+            foreach (int item in result)
+            {
+                Console.Write($"{item} ");
+            }
+           
+            return result;
+        }
+
+        static int[] SortArray(int[]array)
+        {
+            var temp = 0;
+            Console.WriteLine("\nОтсортировать массив");
+            Console.ReadKey();
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int k = i + 1; k < array.Length; k++)
+                {
+                    if (array[i] > array[k])
+                    {
+                        temp = array[k];
+                        array[k] = array[i];
+                        array[i] = temp;
+                    }
+                }
+            }
+            foreach (int item in array)
+            {
+                Console.Write($"{item} ");
+            }
+            return array;
+        }
+        static void Main(string[] args)
+        {
+            int[]Array= GetArrayFromConsole();
+            SortArray(Array);
+            Console.ReadKey();
+        }
+    }
+}
