56a73fed9196d813b2482e1d41f7e28dca41effb
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-21 10:56:32 +0300

    Добавил Exercise_5_3_0

---
+++ Module_5/Exercise_5_3_0/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise_5_3_0
+{
+    internal class Program
+    {
+        static void GetName(string name)
+        {
+            Console.Write("Введите имя: ");
+            name =Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            var someName = "Jack";
+            Console.WriteLine(someName);
+
+            GetName(someName);
+
+            Console.WriteLine(someName);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Module_5/Exercise_5_3_1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercise_5_3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var someAge = 25;
+            Console.WriteLine();
+        }
+    }
+}
