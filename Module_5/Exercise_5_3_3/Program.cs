27a8ccf31d60fb2464c6fc16fa2ef7eaa4d2254a
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-21 11:20:42 +0300

    Добавил Exercise_5_3_5

---
+++ Module_5/Exercise_5_3_3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercise_5_3_3
+{
+    internal class Program
+    {
+        static void GetName(ref string name)
+        {
+            Console.Write("Введите имя: ");
+            name = Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            var someName = "Jack";
+            Console.WriteLine(someName);
+
+            GetName(ref someName);
+
+            Console.WriteLine(someName);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Module_5/Exercise_5_3_5/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercise_5_3_5
+{
+    internal class Program
+    {
+        static void BigDataOperation(int[]arr)
+        {
+            arr[0] = 4;
+        }
+        static void Main(string[] args)
+        {
+            var arr = new[] {1,2,3 };
+            Console.WriteLine(arr[0]);
+            BigDataOperation(arr);
+            Console.WriteLine(arr[0]);
+
+            Console.ReadKey();
+        }
+    }
+}
