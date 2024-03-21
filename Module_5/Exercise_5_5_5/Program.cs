6745241467a688d0896d906a28ce019d55aa10e5
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-21 14:18:52 +0300

    Добавил Exercise_5_5_5

---
+++ Module_5/Exercise_5_5_5/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercise_5_5_5
+{
+    internal class Program
+    {
+        static decimal Factorial(int x)
+        {
+            if (x == 0) 
+                return 1;
+            else
+            {
+                return x*Factorial(x-1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите Факториал: ");
+            int fact=int.Parse(Console.ReadLine());
+            decimal result = Factorial(fact);
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
