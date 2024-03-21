4fdc0fc0d0e4c8cecbdbea09d91ae49bd11d5159
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-21 14:59:44 +0300

    Добавил Exercise_5_5_8

---
+++ Module_5/Exercise_5_5_8/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercise_5_5_8
+{
+    internal class Program
+    {
+        static int PowerUp(int N,byte pow)
+        {
+           
+            if(pow==0) return 1;
+            else if(pow==1) return N;
+            else
+            {
+                return N * (PowerUp(N, --pow));
+                
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int N=int.Parse(Console.ReadLine());
+            Console.Write("Введите степень: ");
+            byte pow = byte.Parse(Console.ReadLine());
+
+            Console.Write($"Ответ: {PowerUp(N,pow)}");
+
+            Console.ReadKey();
+        }
+    }
+}
