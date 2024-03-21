db5dee4e80b8d82ae32eba296ad5a9fe8da2f460
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-21 13:52:52 +0300

    Добавил Exercise_5_5_1

---
+++ Module_5/Exercise_5_5_1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercise_5_5_1
+{
+    internal class Program
+    {
+        static void Echo(string str, int deep)
+        {
+            var modif = str;
+            
+            if (modif.Length>0)
+            {
+                modif =modif.Remove(0,2);
+                Console.WriteLine("..." + modif);
+            }
+
+            if (deep > 1)
+            {
+                Echo(modif, deep - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Напишите что нибудь: ");
+            string str = Console.ReadLine();
+            Console.Write("Глубина эхо: ");
+            int deep = int.Parse(Console.ReadLine());
+
+            Echo(str, deep);
+        }
+    }
+}
---
+++ Module_5/Unit_5_5_0/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Unit_5_5_0
+{
+    internal class Program
+    {
+        static void Echo(string str, int deep)
+        {
+            Console.WriteLine(str);
+
+            if (deep > 1)
+            {
+                Echo(str, deep-1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Напишите что нибудь: ");
+            string str=Console.ReadLine();
+            Console.Write("Глубина эхо: ");
+            int deep = int.Parse(Console.ReadLine());
+            
+            Echo(str, deep);
+        }
+    }
+}
